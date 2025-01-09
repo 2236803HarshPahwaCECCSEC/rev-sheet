fbf3d761d1a7e793cfa66c92bfb24539ca8db71f
HarshPahwa <130906110+2236803HarshPahwaCECCSEC@users.noreply.github.com>
2025-01-09 12:21:19 +0530

    Time: 2525 ms (11.34%) | Memory: 34 MB (94.44%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -11,5 +11,21 @@ public:
             }
         }
         return count;
+        // int n=nums.size();
+        // int l=0;
+        // int r=0;
+        // int count=0;
+        //     int sum=0;
+
+        // while(r<n){
+        //     sum+=nums[r];
+        //     while(sum>k&&l<=r){
+        //         sum-=nums[l];
+        //         l=l+1;
+        //     }
+        //     if(sum==k) count++;
+        //     r=r+1;
+        // }
+        // return count;
     }
 };
