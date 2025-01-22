b9ba10b253e33dd1573d9910fc9e9a02ce488061
HarshPahwa <130906110+2236803HarshPahwaCECCSEC@users.noreply.github.com>
2025-01-22 15:14:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (87.45%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> res;
+        int maxel=*max_element(candies.begin(),candies.end());
+        for(auto el:candies){
+            int sum=el+extraCandies;
+            if(sum>=maxel){
+                res.push_back(true);
+            }else res.push_back(false);
+        }
+        return res;
+    }
+};
