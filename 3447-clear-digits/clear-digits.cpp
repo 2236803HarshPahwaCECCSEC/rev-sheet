d38d72a6c064c3d841c36238826d36c9d50df4ac
HarshPahwa <130906110+2236803HarshPahwaCECCSEC@users.noreply.github.com>
2025-02-10 22:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (56.73%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        int charindex=0;
-        while(charindex<s.size()){
-            if(isdigit(s[charindex])){
-                s.erase(charindex,1);
-                if(charindex>0){
-                    s.erase(charindex-1,1);
-                    charindex--;
-                }
+        // int charindex=0;
+        // while(charindex<s.size()){
+        //     if(isdigit(s[charindex])){
+        //         s.erase(charindex,1);
+        //         if(charindex>0){
+        //             s.erase(charindex-1,1);
+        //             charindex--;
+        //         }
+        //     }else{
+        //         charindex++;
+        //     }
+        // }
+        // return s;
+
+        string ans="";
+        for(int i=0;i<s.size();i++){
+            if(isdigit(s[i])){
+                ans.pop_back();
             }else{
-                charindex++;
+                ans.push_back(s[i]);
             }
         }
-        return s;
+        return ans;
     }
 };
