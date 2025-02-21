bc48dc6505374736ea49722af59e1a29c9883abe
HarshPahwa <130906110+2236803HarshPahwaCECCSEC@users.noreply.github.com>
2025-02-21 23:25:07 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (33.88%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
-        int maxdiff=0;
+        // int maxdiff=0; brute force approach
         
-        for(int i=0;i<nums.size();i++){
-            int diff=0;
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]<nums[j]){
-                    diff=nums[j]-nums[i];
-                    maxdiff=max(maxdiff,diff);
-                }
-            }
+        // for(int i=0;i<nums.size();i++){
+        //     int diff=0;
+        //     for(int j=i+1;j<nums.size();j++){
+        //         if(nums[i]<nums[j]){
+        //             diff=nums[j]-nums[i];
+        //             maxdiff=max(maxdiff,diff);
+        //         }
+        //     }
             
+        // }
+        // return (maxdiff>0)?maxdiff:-1; 
+
+
+        int minVal=nums[0];
+        int maxdiff=-1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>minVal){
+                maxdiff=max(maxdiff,nums[i]-minVal);
+            }else{
+                minVal=nums[i];
+            }
         }
-        return (maxdiff>0)?maxdiff:-1;    
+        return maxdiff;
     }
 };
