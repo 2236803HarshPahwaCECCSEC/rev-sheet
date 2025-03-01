3a5ad1a5b7e884a26e08482f4b36f02d11827a3d
HarshPahwa <130906110+2236803HarshPahwaCECCSEC@users.noreply.github.com>
2025-03-02 00:52:51 +0530

    Time: 3 ms (5.97%) | Memory: 10.7 MB (21.83%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int find(int indx,vector<int>&nums,vector<int>&dp){
+    int find(int indx,vector<int>&nums,vector<int>&dp){//memoization based
         if(indx==0) return nums[0];
         if(indx<1) return 0;
         if(dp[indx]!=-1) return dp[indx];
@@ -12,6 +12,16 @@ public:
         //memoization based approach
         int n=nums.size();
         vector<int>dp(n,-1);
-        return find(n-1,nums,dp);
+        // return find(n-1,nums,dp);
+
+        //tabulation method
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1) take+=dp[i-2];
+            int nottake=0+dp[i-1];
+            dp[i]=max(take,nottake);
+        }
+        return dp[n-1];
     }
 };
