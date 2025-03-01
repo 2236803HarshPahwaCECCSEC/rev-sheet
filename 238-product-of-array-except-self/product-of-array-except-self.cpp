e9f731a78eaac6397ef47fab6ef240382cddc76f
HarshPahwa <130906110+2236803HarshPahwaCECCSEC@users.noreply.github.com>
2025-03-01 13:25:41 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (59.23%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -12,19 +12,32 @@ public:
         //     result[i]=product;
         // }
         // return result;
+        // int n=nums.size();//o(n) sc o(n)
+        // vector<int> prefix(n,1),suffix(n,1),output(n);
+        // for(int i=1;i<n;i++){
+        //     prefix[i]=prefix[i-1]*nums[i-1];
+        // }
+        // for(int i=n-2;i>=0;i--){
+        //     suffix[i]=suffix[i+1]*nums[i+1];
+        // }
+        // for(int i=0;i<n;i++){
+        //     output[i]=prefix[i]*suffix[i];
+        // }
+        // return output;
+
+        //most efficient approach TC o(n) SC o(1)
         int n=nums.size();
-        vector<int> prefix(n,1),suffix(n,1),output(n);
-        for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]*nums[i-1];
-        }
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=suffix[i+1]*nums[i+1];
-        }
+        vector<int>output(n,1);
+        int prefix=1;
         for(int i=0;i<n;i++){
-            output[i]=prefix[i]*suffix[i];
+            output[i]*=prefix;
+            prefix*=nums[i];
+        }
+        int suffix=1;
+        for(int i=n-1;i>=0;i--){
+            output[i]*=suffix;
+            suffix*=nums[i];
         }
         return output;
-
-
     }
 };
