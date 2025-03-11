e6a01d677060c7eb448e1e7d37ff17b89ec9a6d5
HarshPahwa <130906110+2236803HarshPahwaCECCSEC@users.noreply.github.com>
2025-03-11 17:07:12 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (76.17%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-bool binarySearch(vector<int>&nums,int target){
-    int low=0;
-    int high=nums.size()-1;
-    while(low<=high){
-        int mid=low+(high-low)/2;
-        if(nums[mid]==target) return true;
-        else if(nums[mid]<target) low=mid+1;
-        else high=mid-1;
-    }
-    return false;
-}
+// bool binarySearch(vector<int>&nums,int target){
+//     int low=0;
+//     int high=nums.size()-1;
+//     while(low<=high){
+//         int mid=low+(high-low)/2;
+//         if(nums[mid]==target) return true;
+//         else if(nums[mid]<target) low=mid+1;
+//         else high=mid-1;
+//     }
+//     return false;
+// }
+        // for(int i=0;i<m;i++){
+        //     if(matrix[i][0]<=target&&target<=matrix[i][n-1]){
+        //         return binarySearch(matrix[i],target);
+        //     }
+        // }
+        // return false;
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         //brute forceO(m*n) linear search algortihm
 
@@ -23,13 +29,17 @@ bool binarySearch(vector<int>&nums,int target){
         // }
         // return false;
 
-        //second approach using binary search but without flattening
+        //second approach using binary search but with flattening
         int m=matrix.size();
         int n=matrix[0].size();
-        for(int i=0;i<m;i++){
-            if(matrix[i][0]<=target&&target<=matrix[i][n-1]){
-                return binarySearch(matrix[i],target);
-            }
+        int low=0;
+        int high=(m*n)-1;        
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int row=mid/n,col=mid%n;
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]<target)low=mid+1;
+            else high=mid-1;
         }
         return false;
     }
