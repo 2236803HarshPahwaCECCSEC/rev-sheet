a09df0044202a3e43fc653cf3b915e5380e2ded0
HarshPahwa <130906110+2236803HarshPahwaCECCSEC@users.noreply.github.com>
2025-03-13 09:32:20 +0530

    Time: 35 ms (49.18%) | Memory: 21.9 MB (45.91%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -25,13 +25,31 @@ public:
                 }
             }
         }
-            dp[amount] =mini; // Memoize the result
+    dp[amount] =mini; // Memoize the result
     return dp[amount];
+    }
+    int usingTabu(vector<int>& coins,int amount){
+        vector<int>dp(amount+1,INT_MAX);
+        dp[0]=0;//base case when amount is 0 coins choosen would be zero
+        for(int value=1;value<=amount;value++){
+        int mini=INT_MAX;
+            for(int i=0;i<coins.size();i++){
+                if(value-coins[i]>=0){
+                    int ans=dp[value-coins[i]];
+                    if(ans!=INT_MAX){
+                        int result=1+ans;//choose this coin and the previous value term
+                        mini=min(mini,result);
+                    }
+                }
+            }
+            dp[value]=mini;
+        }
+        return dp[amount];
     }
     int coinChange(vector<int>& coins, int amount) {
         // return ansUsingRecursion(coins,amount);
-        vector<int>dp(amount+1,-1);
-        if(usingmemo(coins,amount,dp)==INT_MAX) return -1;
-        else return usingmemo(coins,amount,dp); 
+        // vector<int>dp(amount+1,-1);
+        if(usingTabu(coins,amount)==INT_MAX) return -1;
+        else return usingTabu(coins,amount); 
     }
 };
